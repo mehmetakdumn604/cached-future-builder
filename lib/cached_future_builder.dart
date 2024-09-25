63e723164499abec9d6c94d208d3cab75e809fa4
mehmetakdumn604 <makdumn604@gmail.com>
2024-09-25 15:56:44 +0300

    added comments to understandable clearly

--- lib/cached_future_builder.dart
+++ lib/cached_future_builder.dart
@@ -1,3 +1,7 @@
+/// A Flutter package that manages caching of data fetched from a Future.
+///
+/// This package allows you to cache data for later retrieval,
+/// with support for custom cache durations and force refresh functionality.
 library cached_future_builder;
 
 export 'src/cached_future_widget.dart';
--- lib/src/models/base_model.dart
+++ lib/src/models/base_model.dart
@@ -1,4 +1,27 @@
+/// A base model class for serialization and deserialization.
+///
+/// This class provides methods for converting JSON data to a model instance
+/// and vice versa.
 abstract class BaseModel {
+  /// Converts the BaseModel instance to a JSON map.
+  ///
+  /// This method returns a [Map<String, dynamic>] representation
+  /// of the BaseModel instance.
+  ///
+  /// Example:
+  /// ```dart
+  /// final json = model.toJson();
+  /// ```
   Map<String, dynamic> toJson();
+
+  /// Creates an instance of BaseModel from a JSON map.
+  ///
+  /// This method takes a [Map<String, dynamic>] as input and returns
+  /// an instance of [BaseModel].
+  ///
+  /// Example:
+  /// ```dart
+  /// final model = BaseModel.fromJson(jsonMap);
+  /// ```
   BaseModel fromJson(Map<String, dynamic> json);
 }
